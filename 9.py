a12e83d29922fa1d9466870b53c26442a3adb55e
Ludovic Scholz <lscholz@altra.one>
2022-12-10 16:41:27 +0100

    close enough

--- 9.py
+++ 9.py
@@ -5,18 +5,16 @@ import time
 
 x_max = 5
 y_max = 6
-size = 20
+size = 30
 clear = lambda: os.system('cls')
 
-def print_ht(h,t, t2, visited):
+def print_ht(h,ts, visited):
     for i in range(h[0]-size,h[0]+size):
         for j in range(h[1]-size, h[1]+size):
             if((i,j) == h):
                 print("H", end="")
-            elif((i,j) == t):
-                print("1", end="")
-            elif((i,j) == t2):
-                print("2", end="")
+            elif((i,j) in ts):
+                print(ts.index((i,j))+1, end="")
             elif((i,j) == (0,0)):
                 print("s", end="")
             elif( (i,j) in visited):
@@ -33,61 +31,66 @@ def h_too_far_from_t(h,t):
     distance = (t[0]-h[0])**2 + (t[1]-h[1])**2
     #print("dist", distance)
     return distance > 2
+
+def do_move_tail(t1,t2,hasnt_moved_since, last_move, last_moves_t, pos):
+    if(h_too_far_from_t(t1,t2)):
+        move_tail = (0,0)
+        for m in range(hasnt_moved_since):
+            move_tail = move(move_tail,last_move[-m-1-pos])
+        t2 = move(t2, move_tail)
+        last_moves_t.append(move_tail)
+    return t2, last_moves_t
+
+def check_hasnt_move(ts, ts_hasnt_moved_since,last_ts_pos, i):
+    if(i>9):
+         return
+    if(ts[i] == last_ts_pos[i]):
+        ts_hasnt_moved_since[i] += 1
+    else:
+        ts_hasnt_moved_since[i] = 1
+        check_hasnt_move(ts, ts_hasnt_moved_since,last_ts_pos, i+1)
+    last_ts_pos[i] = ts[i]
+    
 def part1(data):
     h = (0,0)
-    t = (0,0)
-    t2 = (0,0)
-    visited = set()
+    ts = [(0,0)] * 10
+    visited_part1 = set()
+    visited_part2 = set()
     moves = {
         "U": (-1, 0),
         "D": (1, 0),
         "R": (0, 1),
         "L": (0, -1)
     }
-    last_moves = []
-    last_moves_t = []
-    t_hasnt_moved_since = 0
-    t2_hasnt_moved_since = 0
-    last_t_pos = h
-    last_t2_pos = h
+    last_moves_h = []
+    last_moves_ts = []
+    for i in range(10):
+        last_moves_ts.append([(0,0)])
+    ts_hasnt_moved_since = []
+    for i in range(10):
+        ts_hasnt_moved_since.append(0)
+    last_ts_pos = []
+    for i in range(10):
+        last_ts_pos.append(h)
     for line in data:
         dir, n = line.split(" ")
         for i in range(int(n)):
             h = move(h, moves[dir])
-            if(h_too_far_from_t(h,t)):
-                move_t = (0,0)
-                for m in range(t_hasnt_moved_since):
-                    move_t = move(move_t,last_moves[-m-1])
-                t = move(t, move_t)
-                last_moves_t.append(move_t)
-            if(h_too_far_from_t(t,t2)):
-                move_t2 = (0,0)
-                for m in range(t2_hasnt_moved_since):
-                    move_t2 = move(move_t2,last_moves_t[-m-2])
-                t2 = move(t2, move_t2)
+            ts[0],last_moves_ts[0] = do_move_tail(h,ts[0],ts_hasnt_moved_since[0],last_moves_h, last_moves_ts[0], 0)
+            for j in range(1,9):
+                ts[j],last_moves_ts[j]  = do_move_tail(ts[j-1],ts[j],ts_hasnt_moved_since[j],last_moves_ts[j-1], last_moves_ts[j], 1)
                     
+            check_hasnt_move(ts, ts_hasnt_moved_since,last_ts_pos, 0)
             
-            if(t == last_t_pos):
-                t_hasnt_moved_since += 1
-            else:
-                t_hasnt_moved_since = 1
-                if(t2 == last_t2_pos):
-                    t2_hasnt_moved_since += 1
-                else:
-                    t2_hasnt_moved_since = 1
-                last_t2_pos = t2
-            last_t_pos = t
-            
-            
-
-            last_moves.append(moves[dir])
-            visited.add(t2)
-            print_ht(h,t,t2, visited)
-            time.sleep(2)
+            last_moves_h.append(moves[dir])
+            visited_part1.add(ts[0])
+            visited_part2.add(ts[8])
+            print_ht(h,ts, visited_part2)
+            time.sleep(0.2)
             clear()
 
-    print_ht(h,t,t2, visited) 
-    print(len(visited))
+    print_ht(h,ts, visited_part2) 
+    print(len(visited_part2))
         
 
 
